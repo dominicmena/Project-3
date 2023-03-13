2484c97e2ef4aaa54348900c3d973a6cbc74d588
sydney-clark <sydney.natasha.clark@gmail.com>
2023-03-13 10:25:05 -0700

    basic frontend files setup

---
+++ recipe-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './recipe.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Phat to the Max Recipes</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
