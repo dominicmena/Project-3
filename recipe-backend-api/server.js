9b854fa5d84e60e1c6bfacc6f4b16b6f16a6e37e
dominicmena <mena.dominic@gmail.com>
2023-03-13 12:05:47 -0500

    got the server connected to mongoose and all routes set up

--- recipe-backend-api/server.js
+++ recipe-backend-api/server.js
@@ -9,3 +9,27 @@ app.get('/recipes', async (req, res) => {
     const allRecipes = await Recipes.find({})
     res.json(allRecipes)
 })
+
+app.post('/recipes', async (req, res) => {
+    const newRecipe = await Recipes.create(req.body)
+    res.json(newRecipe)
+})
+
+app.put('/animals/:id', async (req, res) => {
+    const updatedRecipe = await Recipes.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    res.json(updatedRecipe)
+})
+
+app.delete('/animals/:id', async (req, res) => {
+    const deleteRecipe = await Recipes.findByIdAndRemove(req.params.id)
+    res.json(deleteRecipe)
+})
+
+app.listen(3000, () => {
+    console.log('listening...')
+})
+
+mongoose.connect('mongodb://localhost:27017/recipemern')
+mongoose.connection.once('open', () => {
+    console.log('connected to mongod...')
+})
