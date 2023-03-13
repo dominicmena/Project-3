5c614747ed25ed1b7e0be29975f3baaddc3cf0a8
dominicmena <mena.dominic@gmail.com>
2023-03-13 12:20:36 -0500

    tested all routes and created test data

--- recipe-backend-api/server.js
+++ recipe-backend-api/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 const Recipes = require('./models/recipeschema')
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/recipes', async (req, res) => {
     const allRecipes = await Recipes.find({})
@@ -15,12 +17,12 @@ app.post('/recipes', async (req, res) => {
     res.json(newRecipe)
 })
 
-app.put('/animals/:id', async (req, res) => {
+app.put('/recipes/:id', async (req, res) => {
     const updatedRecipe = await Recipes.findByIdAndUpdate(req.params.id, req.body, {new:true})
     res.json(updatedRecipe)
 })
 
-app.delete('/animals/:id', async (req, res) => {
+app.delete('/recipes/:id', async (req, res) => {
     const deleteRecipe = await Recipes.findByIdAndRemove(req.params.id)
     res.json(deleteRecipe)
 })
