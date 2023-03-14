158e534bfa9041e5ac68abc082b87a90ad842714
sydney-clark <sydney.natasha.clark@gmail.com>
2023-03-14 10:39:04 -0700

    addded more css

--- recipe-frontend/src/components/indexCards.js
+++ recipe-frontend/src/components/indexCards.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import DeleteButton from './delete'
 import Edit from './edit'
-
+import Card from "react-bootstrap/Card";
 
 
 const IndexComponent = (props) =>{
@@ -13,13 +13,15 @@ const IndexComponent = (props) =>{
     {props.allRecipe.map((recipe)=>{
         return(
             <>
-            <img src={recipe.picture} className="images"/>
-            <h1 clasName="cardTitle">Title: {recipe.title} </h1>
+            <Card>
+            <img src={recipe.picture} className="cardImages"/>
+            <h1 clasName="cardTitle">Title: {recipe.title}</h1>
             <h1 className="cardRecipe">Recipe: {recipe.recipe}</h1>
 
             <DeleteButton delete={props.setAllRecipe}  currentRecipe={recipe} update={props.update} setUpdate={props.setUpdate} updateApp={props.updateApp}/>
             
             <Edit currentRecipe={recipe} update={props.update} setUpdate={props.setUpdate} updateApp={props.updateApp} _id={recipe._id} setAllRecipe={props.setAllRecipe}/>
+            </Card>
             </>
         )
     })}
